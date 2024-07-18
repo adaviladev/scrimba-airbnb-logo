baeeb648bf2d147c3458ee6c474b3df456cafa0e
AlvaroDavilaDev <alvarodaviladev@gmail.com>
2024-07-18 13:32:13 -0400

    first commit

---
+++ components/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <nav>
+            <img class="nav-logo" src="../images/airbnb-logo.png"/> 
+        </nav>
+    )
+}
