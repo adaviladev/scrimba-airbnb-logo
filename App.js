679b979735d30afef51be01ec60cce25622a57b7
AlvaroDavilaDev <alvarodaviladev@gmail.com>
2024-07-18 17:19:47 -0400

    update

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from "react"
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Nav"
 import Hero from "./components/Hero"
 
 export default function App() {
     return (
         <div>
-            <Navbar />
+            <Nav />
             <Hero />
         </div>
     )
